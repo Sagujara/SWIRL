f3e01033b56b29cbd9159abcaba58ef6497c7580
Samuel Gutierrez <s.gutierrezj@uniandes.edu.co>
2022-06-29 12:33:58 -0500

    SECOND LESSON SUBSETTING VECTORS

--- Swirl 101.R
+++ Swirl 101.R
@@ -18,4 +18,24 @@ sum(my_na)
 my_data
 0/0
 Inf - Inf
+#second lesson, here goes the subsetting vectors.
+x <- c(NA, 3, NA, -0.4, NA, 0.33, NA, NA, NA, NA, 2.44, -747, -46463, 836, NA, NA, NA, -0.876, 87, 654, -5432, 19, -3, 4, NA, 1.33, NA, NA, NA, NA, -944, NA, 834539, NA, 94762, NA, NA, -03836, 19847, NA)
+x
+x[1:10]
+x[is.na(x)]
+y <- x[!is.na(x)]
+y
+x[!is.na & x > 0]
+x[!is.na(x) & x > 0]
+x[c(3,5,7)]
+x[0]
+x[3000]
+x[c(-2, -10)]
+vect <- c(foo = 11, bar = 2, norf = NA)
+names(vect)
+vect2 <- c(11, 2, NA)
+names(vect2) <- c("foo", "bar", "norf")
+identical(vect, vect2)
+vect[c("foo", "bar")]
+
 
