ae53d7f00b9a9c09a06c89073b47e0574bbfb06f
Samuel Gutierrez <s.gutierrezj@uniandes.edu.co>
2022-06-29 11:25:45 -0500

    Exercises of creating vectors and finding missing values. Leson 5

--- Swirl 101.R
+++ Swirl 101.R
@@ -2,3 +2,20 @@ install.packages("swirl")
 library(swirl)
 library("tidyverse", lib.loc="/Library/Frameworks/R.framework/Versions/3.6/Resources/library")
 install_from_swirl("R Programming")
+swirl()
+x <- c(44, NA, 5, NA)
+3 * x
+y <- rnorm(1000)
+z <- rep(NA, 1000)
+my_data <- sample(c(y, z), 100)
+my_na <- is.na(my_data)
+print(my_na)
+my_na
+my_data == NA
+y
+y == 1.507985179
+sum(my_na)
+my_data
+0/0
+Inf - Inf
+
