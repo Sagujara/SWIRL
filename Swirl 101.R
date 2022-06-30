de56569707155a34b6b2f72be4606e3e2e635bb5
Samuel Gutierrez <s.gutierrezj@uniandes.edu.co>
2022-06-30 12:59:45 -0500

    logical matrices and dat frame practice

--- Swirl 101.R
+++ Swirl 101.R
@@ -37,5 +37,64 @@ vect2 <- c(11, 2, NA)
 names(vect2) <- c("foo", "bar", "norf")
 identical(vect, vect2)
 vect[c("foo", "bar")]
+#matrices and data frames
+my_vector <- 1:20
+my_vector
+dim(my_vector)
+length(my_vector)
+dim(my_vector) <- c(4,5)
+dim(my_vector)
+attributes(my_vector)
+my_vectir
+my_vector
+class(my_vector)
+my_matrix <- my_vector
+?matrix()
+?matrix
+my_matrix2 <- matrix(1:20, 4, 5)
+identical(my_matrix, my_matrix2)
+patients <- c("Bill", "Gina", "Kelly", "Sean")
+cbind(patients, my_matrix)
+my_data <- data.frame(patients, my_matrix)
+my_data
+class(my_data)
+cnames <- c("patient", "age", "weight", "bp", "rating", "test")
+colnames(my_data) <- cnames
+my_data
+
+#logical
+
+TRUE == TRUE
+TRUE=TRUE
+TRUE = TRUE
+(FALSE == TRUE) == FALSE
+6 == 7
+6 < 7
+10 <= 10
+5 != 7
+5 != 7
+!(5==7)
+FALSE
+FALSE & FALSE
+TRUE & c(TRUE, F, F)
+TRUE & c(TRUE, FALSE, FALSE)
+TRUE && c(TRUE, FALSE, FALSE)
+TRUE | c(TRUE, FALSE, FALSE)
+TRUE || c(TRUE, FALSE, FALSE)
+5 > 8 || 6 != 8 && 4 > 3.9
+TRUE(6 > 4)
+isTRUE(6>4)
+identical('twins', 'twins')
+xor(5 == 6, !FALSE)
+ints <- sample(10)
+ints
+ints <
+d
+ints > 5
+e <- ints >7
+which(ints > 7)
+any(ints < 0)
+all(ints>0)
+
 
 
